50af6c406ca4983237f04383794a6fb6f9283b0e
Nilay Sharma <nilaysharma642@gmail.com>
2022-06-02 23:56:29 +0530

    update dependecies

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
 import Web3 from 'web3';
+import {useEffect, useState} from 'react'
 
 function App() {
   const [account, setAccount] = useState('')
 
   useEffect(() => {
+    let isMounted = true
     const loadBlockChainData = async () => {
       const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
       const accounts = await web3.eth.getAccounts()
-      setAccount(accounts[0] )
+      console.log(accounts)
+      isMounted && setAccount(accounts[0] )
     }
 
     loadBlockChainData()
   
     return () => {
-      second
+      isMounted = false
     }
   }, [])
   
