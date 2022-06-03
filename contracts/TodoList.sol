ea19d956648f04943db0ee4d82d08f3d11f12d9a
Nilay Sharma <nilaysharma642@gmail.com>
2022-06-03 13:17:49 +0530

    add truffle and migrations

---
+++ contracts/TodoList.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+  uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+}
